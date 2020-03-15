8c23f4678d1f565a36bd250afc40662fa720514c
ledyncas95 <62202066+ledyncas95@users.noreply.github.com>
2020-03-15 11:39:41 +0200

    Update Myscript.py

--- Myscript.py
+++ Myscript.py
@@ -1,4 +1,4 @@
-# Import necessary modules 
+#Librerias necesarias
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,7 +12,6 @@ X = iris.data
 y = iris.target
 #Particion 80/20
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20) 
-knn = KNeighborsClassifier(n_neighbors=7)  
 knn = KNeighborsClassifier(n_neighbors=6)
 knn.fit(X_train,y_train)
 
